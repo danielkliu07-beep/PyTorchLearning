35a07c6305b802b70ce9f22f464aba48e59d7c88
danielkliu07-beep <danielkliu07@gmail.com>
2026-06-26 12:29:41 -0400

    Added file

---
+++ word_embedding.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+
+from torch.optim import Adam
+from torch.distributions.uniform import Uniform #Initialize the weights in the network uniformly
+from torch.utils.data import TensorDataset, DataLoader
+
+import lightning as L
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class WordEmbeddingFromScratch(L.LightningModule):
+
+    def __init__(self):
+        super().__init__()
+
+        min_value = -0.5
+        max_value = 0.5
+
+        self.input1_w1 = nn.Parameter(Uniform(min_value, max_value).sample()) #Initializes w1 to a random number between -0.5 and 0.5
+        
+        self.input1_w2 = nn.Parameter(Uniform(min_value, max_value).sample())
+        self.input2_w1 = nn.Parameter(Uniform(min_value, max_value).sample())
+        self.input2_w2 = nn.Parameter(Uniform(min_value, max_value).sample())
+        self.input3_w1 = nn.Parameter(Uniform(min_value, max_value).sample())
+        self.input3_w2 = nn.Parameter(Uniform(min_value, max_value).sample())
+        self.input4_w1 = nn.Parameter(Uniform(min_value, max_value).sample())
+        self.input4_w2 = nn.Parameter(Uniform(min_value, max_value).sample())
+
+        self.output1_w1 = nn.Parameter(Uniform(min_value, max_value).sample()) 
+        self.output1_w2 = nn.Parameter(Uniform(min_value, max_value).sample())
+        self.output2_w1 = nn.Parameter(Uniform(min_value, max_value).sample())
+        self.output2_w2 = nn.Parameter(Uniform(min_value, max_value).sample())
+        self.output3_w1 = nn.Parameter(Uniform(min_value, max_value).sample())
+        self.output3_w2 = nn.Parameter(Uniform(min_value, max_value).sample())
+        self.output4_w1 = nn.Parameter(Uniform(min_value, max_value).sample())
+        self.output4_w2 = nn.Parameter(Uniform(min_value, max_value).sample())
+
+        self.loss = nn.CrossEntropyLoss()
+
+
+    def forward(self, input):
+        pass
+
+    def configure_optimizers(self):
+        pass
+
+    def trainign_step(self, batch, batch_idx):
+        pass
+
+
+inputs = torch.tensor([[1., 0., 0., 0.],
+                       [0., 1., 0., 0.],
+                       [0., 0., 1., 0.],
+                       [0., 0., 0., 1.]])
+
+labels = torch.tensor([[0., 1., 0., 0.],
+                       [0., 0., 1., 0.],
+                       [0., 0., 0., 1.],
+                       [0., 1., 0., 0.]])
+
+dataset = TensorDataset(inputs, labels)
+dataloader = DataLoader(dataset)
